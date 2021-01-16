d5ee4684991a3c79483afec6c119d9330d942e08
Carlos Bermejo Pérez <carlosbermejop@gmail.com>
2021-01-16 14:33:28 +0100

    Added support for password as arg

--- passwordManagement.js
+++ passwordManagement.js
@@ -1,10 +1,21 @@
 let keyFile = require("./key.json");
 let key = keyFile["key"];
 let CryptoJS = require("crypto-js");
-console.log(key);
-let password = "admin";
+let password;
+console.log(`Your key is: ${key}.`);
+for (let i = 2; i < process.argv.length; i++) {
+    
+    if (process.argv[i].split("=").length == 2 && process.argv[i].split("=")[0] == "--password") {
+        process.env.PASSWORD = process.argv[i].split("=")[1];
+    }
+}
+if (process.env.PASSWORD) {
+    password = process.env.PASSWORD;
+} else {
+    password = "admin";
+}
 let encryptedPassword = CryptoJS.AES.encrypt(password, key).toString();
-console.log(encryptedPassword);
+console.log(`This is the encrypted password you need to copy: ${encryptedPassword}`);
 
 let decryptedPassword = CryptoJS.AES.decrypt(encryptedPassword, key).toString(CryptoJS.enc.Utf8);
-console.log(decryptedPassword);
+console.log(`This is what WebdriverIO will write: ${decryptedPassword}`);
